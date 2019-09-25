0ea08f84f3b8a01bdae529cdf1ede2ebb18a452b
svartalf <self@svartalf.info>
2019-09-25 16:11:08 +0300

    A little bit more code for coverage reports

--- example/src/lib.rs
+++ example/src/lib.rs
@@ -18,15 +18,34 @@ pub fn pid() -> Pid {
     }
 }
 
+#[allow(unused)]
+fn angry_clippy_noises() {
+    let data = vec![1, 2, 3];
+    assert!(data.len() < 0);
+
+    let mut a = 1;
+    let mut b = 2;
+
+    // Look, ma, no hands!
+    a = b;
+    b = a;
+}
+
 #[cfg(test)]
 mod tests {
     use std::process;
 
-    use super::pid;
+    use super::{pid, angry_clippy_noises};
 
     #[test]
     fn test_pid_is_working() {
         assert_eq!(pid() as u32, process::id());
+    }
+
+    #[test]
+    #[should_panic]
+    fn test_clippy_patience() {
+        angry_clippy_noises();
     }
 
 }
