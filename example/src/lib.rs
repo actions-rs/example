80f8ae7a9e86532f0da01e81a46f99ddb7fff61c
svartalf <self@svartalf.info>
2019-10-22 14:20:04 +0300

    Triggering build

--- example/src/lib.rs
+++ example/src/lib.rs
@@ -6,16 +6,12 @@ pub type Pid = example_sys::windows::DWORD;
 
 #[cfg(unix)]
 pub fn pid() -> Pid {
-    unsafe {
-        example_sys::unix::getpid()
-    }
+    unsafe { example_sys::unix::getpid() }
 }
 
 #[cfg(target_os = "windows")]
 pub fn pid() -> Pid {
-    unsafe {
-        example_sys::windows::GetCurrentProcessId()
-    }
+    unsafe { example_sys::windows::GetCurrentProcessId() }
 }
 
 // Specifically dumb function, just to trigger clippy
